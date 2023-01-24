db657dc45f1b5171aa0d7e1f93210ac31147214d
gbe-dev <fred.arnal@gmail.com>
2023-01-24 17:19:41 +0000

    Fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,16 +14,12 @@ class MyDocument extends Document {
   }
 
   render() {
-    const description =
-      'Reservoir Market is an open source NFT marketplace built with Reservoir.'
+    const description = 'Fellowship champions the future of photography. We present a new way to discover, collect and learn about photography by exhibiting NFTs from marquee estates, living artists and emerging talent.'
     const ogImage = 'https://marketplace.reservoir.tools/og-image.png'
     return (
       <Html>
         <Head>
-          <style
-            id="stitches"
-            dangerouslySetInnerHTML={{ __html: getCssText() }}
-          />
+          <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
         </Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -49,16 +45,11 @@ class MyDocument extends Document {
           name="twitter:title"
           content="Fellowship | Marketplace"
         />
-<<<<<<< HEAD
         <meta
           name="twitter:description"
-          content="favicon.png"
+          content="Fellowship champions the future of photography. We present a new way to discover, collect and learn about photography by exhibiting NFTs from marquee estates, living artists and emerging talent."
         />
         <meta name="twitter:site" content="@fellowshiptrust" />
-=======
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:site" content="@reservoir0x" />
->>>>>>> upstream/main
 
         {/* OG - https://ogp.me/ */}
         {/* https://www.opengraph.xyz/ */}
@@ -69,26 +60,15 @@ class MyDocument extends Document {
         <meta property="og:type" content="website" />
         <meta property="og:determiner" content="the" />
         <meta property="og:locale" content="en" />
-<<<<<<< HEAD
         <meta
           property="og:description"
           content="Fellowship champions the future of photography. We present a new way to discover, collect and learn about photography by exhibiting NFTs from marquee estates, living artists and emerging talent."
         />
-=======
-        <meta property="og:description" content={description} />
->>>>>>> upstream/main
-        {/* The optimal size is 1200 x 630 (1.91:1 ratio). */}
         <meta property="og:image" content={ogImage} />
         <meta property="og:image:type" content="image/png" />
-<<<<<<< HEAD
         <meta property="og:image:width" content="1280" />
         <meta property="og:image:height" content="640" />
         <meta property="og:image:alt" content="Fellowship Market Banner" />
-=======
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="Reservoir Market Banner" />
->>>>>>> upstream/main
 
         {/* Reservoir Meta Tags: https://docs.reservoir.tools/docs/reservoir-meta-tags */}
         <meta property="reservoir:title" content="Fellowship Marketplace" />
