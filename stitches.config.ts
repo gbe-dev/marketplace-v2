692bcfd581f7ae66b104e05fe2e025e6e9e6eb94
gbe-dev <fred.arnal@gmail.com>
2023-01-24 23:13:32 +0000

    Reverting back

--- stitches.config.ts
+++ stitches.config.ts
@@ -106,7 +106,7 @@ export const { createTheme, keyframes, styled, globalCss, getCssText } =
       fontSizes: {},
       fontWeights: {},
       fonts: {
-        body: 'MonumentGrotesk',
+        body: 'Inter',
         button: '$body',
       },
       lineHeights: {},
