94eb4fbac104f50d82206f50ec8bfecd28e70aab
gbe-dev <fred.arnal@gmail.com>
2023-01-27 10:33:27 +0000

    Testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -140,9 +140,9 @@ function MyApp({
             // Note that you should at the very least configure the source with your own domain
             apiBase: `${baseUrl}${marketplaceChain.proxyApi}`,
             apiKey: process.env.NEXT_PUBLIC_RESERVOIR_API_KEY,
-            //source: 'marketplace.fellowship.xyz',
-            marketplaceFee: Number(process.env.NEXT_PUBLIC_MP_FEE_BPS),
-            marketplaceFeeRecipient: process.env.NEXT_PUBLIC_MP_FEE_RECIPIENT,
+            source: 'marketplace.fellowship.xyz',
+            //marketplaceFee: 200,
+            //marketplaceFeeRecipient: '0xd7dD9612A21F7C249Fb7F33E9C2e9144345e162b',
             normalizeRoyalties: NORMALIZE_ROYALTIES,
           }}
           theme={reservoirKitTheme}
