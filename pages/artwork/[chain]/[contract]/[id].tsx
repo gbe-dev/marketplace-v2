04cad178d975b1bdeb0435d343c49eebe1df941a
gbe-dev <fred.arnal@gmail.com>
2023-01-28 21:41:34 +0000

    Testing

--- pages/artwork/[chain]/[contract]/[id].tsx
+++ pages/artwork/[chain]/[contract]/[id].tsx
@@ -87,9 +87,7 @@ const IndexPage: NextPage<Props> = ({ id, collectionId, ssr }) => {
   const flagged = useTokenOpenseaBanned(collectionId, id)
   const token = tokens && tokens[0] ? tokens[0] : undefined
   const hrFile = useTokenHR(contract, token?.token?.tokenId)
-  if (token) {
-    token.image = hrFile.hrFile
-  }
+  console.log(Object.keys(token))
   const checkUserOwnership = token?.token?.kind === 'erc1155'
 
   const { data: userTokens } = useUserTokens(
