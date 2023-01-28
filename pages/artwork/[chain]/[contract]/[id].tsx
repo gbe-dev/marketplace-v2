55dc0ab66c324c8d95d6f7e5e7e87aa3e44b9ce1
gbe-dev <fred.arnal@gmail.com>
2023-01-28 17:10:07 +0000

    just for the sake

--- pages/artwork/[chain]/[contract]/[id].tsx
+++ pages/artwork/[chain]/[contract]/[id].tsx
@@ -423,6 +423,131 @@ const IndexPage: NextPage<Props> = ({ id, collectionId, ssr }) => {
           )}
         </Flex>
       </Flex>
+      <>
+  <style
+    dangerouslySetInnerHTML={{
+      __html:
+        "    \n    --six-site-max-width: 3300px;\n    --six-cols: 12;\n    --six-site-margin: 60px;\n    --six-gutter: 48px;\n    --six-panel-width: 85vw;\n    --six-filter-width: 220px;\n    --six-filter-offset: -440px;\n    --six-card-border: 10px;\n    --six-animation-scale-factor: 60px;\n    --six-card-caption: 115px;\n    --scale-amount: 0;\n    --vh: 100vh;\n    --scrollbar-width: 0px;\n    --wp--preset--color--black: #000000;\n    --wp--preset--color--cyan-bluish-gray: #abb8c3;\n    --wp--preset--color--white: #ffffff;\n    --wp--preset--color--pale-pink: #f78da7;\n    --wp--preset--color--vivid-red: #cf2e2e;\n    --wp--preset--color--luminous-vivid-orange: #ff6900;\n    --wp--preset--color--luminous-vivid-amber: #fcb900;\n    --wp--preset--color--light-green-cyan: #7bdcb5;\n    --wp--preset--color--vivid-green-cyan: #00d084;\n    --wp--preset--color--pale-cyan-blue: #8ed1fc;\n    --wp--preset--color--vivid-cyan-blue: #0693e3;\n    --wp--preset--color--vivid-purple: #9b51e0;\n    --wp--preset--color--red: #e27f7f;\n    --wp--preset--color--grey: #959595;\n    --wp--preset--gradient--vivid-cyan-blue-to-vivid-purple: linear-gradient(135deg,rgba(6,147,227,1) 0%,rgb(155,81,224) 100%);\n    --wp--preset--gradient--light-green-cyan-to-vivid-green-cyan: linear-gradient(135deg,rgb(122,220,180) 0%,rgb(0,208,130) 100%);\n    --wp--preset--gradient--luminous-vivid-amber-to-luminous-vivid-orange: linear-gradient(135deg,rgba(252,185,0,1) 0%,rgba(255,105,0,1) 100%);\n    --wp--preset--gradient--luminous-vivid-orange-to-vivid-red: linear-gradient(135deg,rgba(255,105,0,1) 0%,rgb(207,46,46) 100%);\n    --wp--preset--gradient--very-light-gray-to-cyan-bluish-gray: linear-gradient(135deg,rgb(238,238,238) 0%,rgb(169,184,195) 100%);\n    --wp--preset--gradient--cool-to-warm-spectrum: linear-gradient(135deg,rgb(74,234,220) 0%,rgb(151,120,209) 20%,rgb(207,42,186) 40%,rgb(238,44,130) 60%,rgb(251,105,98) 80%,rgb(254,248,76) 100%);\n    --wp--preset--gradient--blush-light-purple: linear-gradient(135deg,rgb(255,206,236) 0%,rgb(152,150,240) 100%);\n    --wp--preset--gradient--blush-bordeaux: linear-gradient(135deg,rgb(254,205,165) 0%,rgb(254,45,45) 50%,rgb(107,0,62) 100%);\n    --wp--preset--gradient--luminous-dusk: linear-gradient(135deg,rgb(255,203,112) 0%,rgb(199,81,192) 50%,rgb(65,88,208) 100%);\n    --wp--preset--gradient--pale-ocean: linear-gradient(135deg,rgb(255,245,203) 0%,rgb(182,227,212) 50%,rgb(51,167,181) 100%);\n    --wp--preset--gradient--electric-grass: linear-gradient(135deg,rgb(202,248,128) 0%,rgb(113,206,126) 100%);\n    --wp--preset--gradient--midnight: linear-gradient(135deg,rgb(2,3,129) 0%,rgb(40,116,252) 100%);\n    --wp--preset--duotone--dark-grayscale: url('#wp-duotone-dark-grayscale');\n    --wp--preset--duotone--grayscale: url('#wp-duotone-grayscale');\n    --wp--preset--duotone--purple-yellow: url('#wp-duotone-purple-yellow');\n    --wp--preset--duotone--blue-red: url('#wp-duotone-blue-red');\n    --wp--preset--duotone--midnight: url('#wp-duotone-midnight');\n    --wp--preset--duotone--magenta-yellow: url('#wp-duotone-magenta-yellow');\n    --wp--preset--duotone--purple-green: url('#wp-duotone-purple-green');\n    --wp--preset--duotone--blue-orange: url('#wp-duotone-blue-orange');\n    --wp--preset--font-size--small: 13px;\n    --wp--preset--font-size--medium: 20px;\n    --wp--preset--font-size--large: 36px;\n    --wp--preset--font-size--x-large: 42px;\n    --wp--preset--spacing--20: 0.44rem;\n    --wp--preset--spacing--30: 0.67rem;\n    --wp--preset--spacing--40: 1rem;\n    --wp--preset--spacing--50: 1.5rem;\n    --wp--preset--spacing--60: 2.25rem;\n    --wp--preset--spacing--70: 3.38rem;\n    --wp--preset--spacing--80: 5.06rem;\n    text-rendering: optimizeSpeed;\n    -webkit-font-smoothing: antialiased;\n    font-style: normal;\n    font-family: ABCMonGrotesk,sans-serif;\n    font-weight: 400;\n    line-height: 1.23077;\n    --line-height: 1.23077;\n    font-size: calc(.552vw + 14.96552px);\n    letter-spacing: 0;\n    color: #191919;\n    box-sizing: border-box;\n    position: relative;\n    width: 100%;\n    min-height: 55vh;\n    padding-left: var(--six-site-margin);\n    padding-right: var(--six-site-margin);\n    display: flex;\n    align-content: space-between;\n    overflow: hidden;\n    background-color: #ef84e3;\n    background-image: linear-gradient(90deg,#ef84e3,#afcdf7);\n\n"
+    }}
+  />
+  <footer className="GlobalFooter " g-component="GlobalFooter" g-options="[]">
+    <div
+      className="GlobalFooter__inner"
+      g-ref="footerInner"
+      style={{
+        translate: "none",
+        rotate: "none",
+        scale: "none",
+        opacity: 0,
+        transform: "translate(0px, -200px)"
+      }}
+    >
+      <div className="GlobalFooter__inner-form">
+        <p>
+          Join our mailing list for updates on
+          <strong> artists, releases and more.</strong>
+        </p>
+        <div className="form-wrapper">
+          {/* Mailchimp for WordPress v4.9.0 - https://wordpress.org/plugins/mailchimp-for-wp/ */}
+          <form
+            id="mc4wp-form-1"
+            className="mc4wp-form mc4wp-form-3558 mc4wp-ajax"
+            method="post"
+            data-id={3558}
+            data-name="Newsletter"
+          >
+            <div className="mc4wp-form-fields">
+              <input
+                type="email"
+                name="EMAIL"
+                placeholder="Email"
+                required=""
+              />
+              <input type="submit" defaultValue="Subscribe" />
+            </div>
+            <label style={{ display: "none !important" }}>
+              Leave this field empty if you're human:{" "}
+              <input
+                type="text"
+                name="_mc4wp_honeypot"
+                defaultValue=""
+                tabIndex={-1}
+                autoComplete="off"
+              />
+            </label>
+            <input
+              type="hidden"
+              name="_mc4wp_timestamp"
+              defaultValue={1674921367}
+            />
+            <input type="hidden" name="_mc4wp_form_id" defaultValue={3558} />
+            <input
+              type="hidden"
+              name="_mc4wp_form_element_id"
+              defaultValue="mc4wp-form-1"
+            />
+            <div className="mc4wp-response" />
+          </form>
+          {/* / Mailchimp for WordPress Plugin */}
+        </div>
+      </div>
+      <div className="GlobalFooter__inner-links">
+        <div className="link-column">
+          <p>©Fellowship 2023</p>
+          <p>All Rights Reserved</p>
+        </div>
+        <div className="link-column">
+          <h3>Follow</h3>
+          <ul>
+            <li>
+              <a
+                href="https://twitter.com/fellowshiptrust"
+                data-no-swup=""
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Twitter,
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://discord.gg/fellowshiptrust"
+                data-no-swup=""
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Discord,
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/fellowship.xyz/?hl=en"
+                data-no-swup=""
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="link-column page-links no-links" />
+        <div className="link-column">
+          <h3>Made By:</h3>
+          <a
+            data-no-swup=""
+            href="https://www.madebysix.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            SIX
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</>
+
     </Layout>
   )
 }
