cc0d6b25b1e78a78face0cfc4ee04e9e69b8372f
gbe-dev <fred.arnal@gmail.com>
2023-01-30 18:02:40 +0000

    Testing protecting OS calls

--- hooks/useTokenExtra.ts
+++ hooks/useTokenExtra.ts
@@ -12,10 +12,18 @@ export default (address?: string, tokenId?: string, chainId: number = 1) => {
       revalidateOnReconnect: false,
     }
   )
+  let hrFile, token_metadata, osName = ""
+
+  if (response.sucess = "true")
+  {
+    hrFile= response.data?.image_original_url ? response.data?.image_original_url : ''
+    token_metadata= response.data?.token_metadata ? response.data?.token_metadata : ''
+    osName= response.data?.owner?.user?.username ? response.data?.owner?.user?.username : ''
+  }
   return {
     ...response,
-    hrFile: response.data?.image_original_url,
-    token_metadata: response.data?.token_metadata,
-    osName: response.data?.owner?.user?.username,
+    hrFile: hrFile,
+    token_metadata: token_metadata,
+    osName: osName,
   }
 }
