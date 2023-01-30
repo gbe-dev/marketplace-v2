b522cfa333fa92b3b20f6a4e66049ad5a69da8f9
gbe-dev <fred.arnal@gmail.com>
2023-01-30 16:09:11 +0000

    Fix

--- pages/artwork/[chain]/[contract]/[id].tsx
+++ pages/artwork/[chain]/[contract]/[id].tsx
@@ -98,7 +98,7 @@ const IndexPage: NextPage<Props> = ({ id, collectionId, ssr }) => {
     osName = tokenExtra.osName
   }
 console.log("ENS" + ownerFormatted)
-console.log("OS" + osName
+console.log("OS" + osName)
 
   const checkUserOwnership = token?.token?.kind === 'erc1155'
 
